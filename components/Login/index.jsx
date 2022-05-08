b383a4c243773dbcf138f3ffa93d8915e82f0ae5
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-05-07 21:46:11 -0300

    create login page

--- components/Login/index.jsx
+++ components/Login/index.jsx
@@ -1,16 +1,24 @@
 import styles from './styles.module.scss'
 
 const Login = () => {
+  const handleSubmit = () => {}
+
   return (
-    <form className={`${styles.card}`}>
-      <div style={{ marginTop: '-80px', marginBottom: '30px' }}>
-        <h1 style={{ marginBottom: '0' }}>Welcome!</h1>
-        <p>Enter your credentials to access your account</p>
-      </div>
-      <input placeholder="Username" style={{ margin: '10px 0px 10px 0px' }} />
-      <input placeholder="Password" style={{ margin: '10px 0px 30px 0px' }} />
-      <button>Login</button>
-    </form>
+    <div className={styles.container}>
+      <form className={`${styles.card}`} onSubmit={handleSubmit}>
+        <header className={`${styles.headerText}`}>
+          <h1 className={`${styles.title}`}>Welcome back</h1>
+          <p className={`${styles.subtitle}`}>
+            Enter your credentials to access your account
+          </p>
+        </header>
+        <div>
+          <input placeholder="Username" className={styles.input} />
+          <input placeholder="Password" className={styles.input} />
+        </div>
+        <button className={`${styles.button}`}>Sign In</button>
+      </form>
+    </div>
   )
 }
 
