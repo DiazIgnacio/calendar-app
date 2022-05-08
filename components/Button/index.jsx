61349c79e130c4444763db549a99508d8be18282
Ignacio Diaz <diaz.ignacio023@gmail.com>
2022-05-08 13:37:07 -0300

    remove log

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -1,7 +1,6 @@
 import styles from './styles.module.scss'
 
 const Button = ({ children, outlined, ...props }) => {
-  console.log(outlined)
   return (
     <button
       className={`${styles.button} ${outlined && styles.outlined}`}
